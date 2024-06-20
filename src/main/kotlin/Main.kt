84bb5894d6a6a7e009b43fe0e40af83e92be883a
Aleksandra Zueva <aleksandra.zueva@exness.com>
2024-06-20 09:47:19 +0300

    add Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
+import menu.ArchiveListMenu
+import java.util.Scanner
+
+val scanner = Scanner(System.`in`)
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    println("Программа для хранения заметок")
+    println("Важно! Для перемещения по пунктам меню использует цифру указанную напротив пункта меню")
+
+    ArchiveListMenu.showMenu()
 }
